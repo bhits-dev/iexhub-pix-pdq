78b512a7e33ebae72e5eba5a9d9eefe4adf41458
sadhch <sadhana.chandravanka@feisystems.com>
2017-08-15 09:42:53 -0400

    Add default spring boot main class and its test

---
+++ iexhub-pix-pdq/src/main/java/gov/samhsa/c2s/IexhubPixPdqApplication.java
@@ -0,0 +1,12 @@
+package gov.samhsa.c2s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IexhubPixPdqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IexhubPixPdqApplication.class, args);
+	}
+}
---
+++ iexhub-pix-pdq/src/test/java/gov/samhsa/c2s/IexhubPixPdqApplicationTests.java
@@ -0,0 +1,16 @@
+package gov.samhsa.c2s;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IexhubPixPdqApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
