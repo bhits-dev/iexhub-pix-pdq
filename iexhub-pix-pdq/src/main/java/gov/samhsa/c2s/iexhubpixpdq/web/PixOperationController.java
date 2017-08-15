01d937a74420d723878fffa3cc024c4cbd11328b
sadhch <sadhana.chandravanka@feisystems.com>
2017-08-15 17:26:02 -0400

    Add restful endpoints for revise and query eid of a person
    #14862 - Start a new Spring Boot project for IExHub and Create Restful services for PIX Add and PIX Update

--- iexhub-pix-pdq/src/main/java/gov/samhsa/c2s/iexhubpixpdq/web/PixOperationController.java
+++ iexhub-pix-pdq/src/main/java/gov/samhsa/c2s/iexhubpixpdq/web/PixOperationController.java
@@ -1,17 +1,23 @@
 package gov.samhsa.c2s.iexhubpixpdq.web;
 
 import gov.samhsa.c2s.iexhubpixpdq.service.PixOperationService;
+import gov.samhsa.c2s.pixclient.util.PixManagerBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping("/pix/persons")
 public class PixOperationController {
+
     @Autowired
     PixOperationService pixOperationService;
 
@@ -22,5 +28,17 @@ public class PixOperationController {
         return pixAddMessage;
     }
 
+    @PutMapping
+    @ResponseStatus(HttpStatus.OK)
+    public String revisePerson(@RequestBody String reqXMLPath){
+        String pixReviseMessage = pixOperationService.updatePerson(reqXMLPath);
+        return pixReviseMessage;
+    }
 
+    @PostMapping("/eid")
+    @ResponseStatus(HttpStatus.OK)
+    public String getPersonEid(@RequestBody String reqXMLPath){
+        String eid = pixOperationService.getPersonEid(reqXMLPath);
+        return eid;
+    }
 }
